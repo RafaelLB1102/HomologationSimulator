7382ea467de511a3407683932e2946c8f2fa31c9
Rafael Londoño Botero <rafael.boterob@opitech.com.co>
2024-05-07 21:31:58 -0500

    Added the font specified in the brand guideline page

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const technologies = []
+const technologies = ["prueba","prueba2"]
+const majorList = ["Ing. Sistemas","Ing. Mecánica"]
+document.addEventListener("DOMContentLoaded", function(){
+    const techDropdown = document.getElementById("TechDropDown")
+    const majorDropdown = document.getElementById("MajorsDropDown")
+    technologies.forEach((technology) => {
+        const element = document.createElement("option")
+        element.text = technology
+        techDropdown.add(element)
+    })
+    majorList.forEach((major) => {
+        const element = document.createElement("option")
+        element.text = major
+        majorDropdown.add(element)
+    })
+})
